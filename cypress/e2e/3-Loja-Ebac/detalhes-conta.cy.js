afda421a956256b9226a58a469707135707486bf
Lucas Gabriel <Lukinhascop1@gmail.com>
2025-03-27 09:47:42 -0300

    Aplicando massa de dados e variaveis

--- cypress/e2e/3-Loja-Ebac/detalhes-conta.cy.js
+++ cypress/e2e/3-Loja-Ebac/detalhes-conta.cy.js
@@ -6,10 +6,8 @@ describe('Funcionalidade: Detalhes da conta', () => {
         cy.visit('minha -conta/edit-account/')
         cy.fixture('perfil').then(login => {
         cy.login(login.usuario, login.senha)
-
-        })
-        
-
+       
+    })
     });
 
 it('Deve completar detalhes da conta com sucesso', () => {
--- cypress/e2e/3-Loja-Ebac/produtos.cy.js
+++ cypress/e2e/3-Loja-Ebac/produtos.cy.js
@@ -1,19 +1,57 @@
 /// <reference types="cypress"/>
+import produtosPage from "../../support/page-objects/produtos.page";
 
 describe('Funcionalidade: Produtos', () => {
 
     beforeEach(() => {
-        cy.visit('produtos')
+        produtosPage.visitarUrl()
     });
     
     it('Deve selecionar um produto da lista ', () => {
         cy.get('.products > .row')
-            //.first()
-            //.last()
-            //.eq(2)
-            .contains('Apollo Running Short')
-            .click()
+           produtosPage.buscarProdutoLista('Arcadio Gym Short')
             cy.get('#tab-title-description > a').should('contain' , 'Descrição')
             
     });
+
+    it('Deve buscar um produto com sucesso', () => {
+        let produto = 'Apollo Running Short'
+        produtosPage.buscarProduto(produto)
+        cy.get('.product_title').should('contain', produto)
+    });
+
+    it('Deve visitar a pagina do produto', () => {
+        produtosPage.visitarProduto('atlas fitness tank')
+        cy.get('.product_title').should('contain', 'Atlas Fitness Tank')
+    });
+    
+    it('Deve adicionar produto ao carrinho', () => {
+        let qtd = 7
+        produtosPage.buscarProduto('Abominable Hoodie')
+        produtosPage.addProdutoCarrinho('L', 'Blue', qtd)
+
+        cy.get('.woocommerce-message').should('contain',  qtd + ' × “Abominable Hoodie” foram adicionados no seu carrinho.')
+
+
+        
+    });
+
+    it.only('Deve adicionar produto ao carrinho buscando da massa de dados', () => {
+        cy.fixture('produtos').then(dados => {
+        produtosPage.buscarProduto(dados[0].nomeProduto)
+        produtosPage.addProdutoCarrinho(
+            dados[0].tamanho,
+            dados [0].cor,
+            dados [0].quantidade)
+
+        cy.get('.woocommerce-message').should('contain', dados [0].nomeProduto)
+
+        })
+        
+        
+
+
+    });
+    
 });
+
